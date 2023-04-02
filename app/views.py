22c4183c9c9c84a9496278454d4f8a625a23672e
Fran Osores <fmosoresortiz@gmail.com>
2023-04-01 23:43:37 -0300

    ADD post and get endpoints to users and projects

--- app/views.py
+++ app/views.py
@@ -20,6 +20,7 @@ db = SQLAlchemy(app)
 
 
 #Models
+#TODO: move this to a models.py file
 class Institution(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
@@ -30,6 +31,30 @@ class Institution(db.Model):
     def __repr__(self):
         return f"Institution(id={self.id}, name='{self.name}', description='{self.description}', creation_date='{self.creation_date}')"
 
+class CustomUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    lastnames = db.Column(db.String(200))
+    rut = db.Column(db.String(200))
+    birth_date = db.Column(db.DateTime)
+    position = db.Column(db.String(200))
+    age = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"CustomUser(id={self.id}, first_name='{self.first_name}', lastnames='{self.lastnames}', rut='{self.rut}', birth_date='{self.birth_date}', position='{self.position}', age='{self.age}')"
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200))
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    responsible = db.Column(db.Integer, db.ForeignKey('custom_user.id'))
+    institution_id = db.Column(db.Integer, db.ForeignKey('institution.id'))
+
+    def __repr__(self):
+        return f"Project(id={self.id}, name='{self.name}', description='{self.description}', start_date='{self.start_date}', end_date='{self.end_date}', responsible='{self.responsible}', institution_id='{self.institution_id}')"
+
 
 def get_db_connection():
     conn = psycopg2.connect(
@@ -38,20 +63,6 @@ def get_db_connection():
         user="postgres",
         password="postgres", )
     return conn
-
-@app.route('/test')
-def home():
-   conn = get_db_connection()
-   cur = conn.cursor()
-   cur.execute('SELECT * FROM institution;')
-   inst = cur.fetchall()
-   cur.close()
-   conn.close()
-   return render_template('index.html', inst=inst)
-
-@app.route('/')
-def index():
-    return "Hello, World!"
 
 
 @app.route('/institutions', methods=['GET', 'POST'])
@@ -133,3 +144,88 @@ def institution(id):
             return json_response(result=True)
         else:
             return json_response(description='Institution not found')
+
+@app.route('/users', methods=['GET', 'POST'])
+def user():
+    if request.method == 'GET':
+        users = CustomUser.query.all()
+        users_list = []
+
+        for user in users:
+            user_dict = {
+                'id': user.id,
+                'first_name': user.first_name,
+                'lastnames': user.lastnames,
+                'rut': user.rut,
+                'birth_date': user.birth_date,
+                'position': user.position,
+                'age': user.age
+            }
+            users_list.append(user_dict)
+
+        return jsonify(users_list)
+
+    elif request.method == 'POST':
+        first_name = request.form['first_name']
+        lastnames = request.form['lastnames']
+        rut = request.form['rut']
+        birth_date = request.form['birth_date']
+        position = request.form['position']
+        age = request.form['age']
+        user = CustomUser(first_name=first_name, lastnames=lastnames, rut=rut, birth_date=birth_date, position=position, age=age)
+        db.session.add(user)
+        db.session.commit()
+
+        user_dict = {
+            'id': user.id,
+            'first_name': user.first_name,
+            'lastnames': user.lastnames,
+            'rut': user.rut,
+            'birth_date': user.birth_date,
+            'position': user.position,
+            'age': user.age
+        }
+
+        return jsonify(user_dict)
+
+@app.route('/projects', methods=['GET', 'POST'])
+def project():
+    if request.method == 'GET':
+        projects = Project.query.all()
+        projects_list = []
+
+        for project in projects:
+            project_dict = {
+                'id': project.id,
+                'name': project.name,
+                'description': project.description,
+                'start_date': project.start_date,
+                'end_date': project.end_date,
+                'responsible': project.responsible,
+                'institution_id': project.institution_id
+            }
+            projects_list.append(project_dict)
+
+        return jsonify(projects_list)
+    elif request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        start_date = request.form['start_date']
+        end_date = request.form['end_date']
+        responsible = request.form['responsible']
+        institution_id = request.form['institution_id']
+        project = Project(name=name, description=description, start_date=start_date, end_date=end_date, responsible=responsible, institution_id=institution_id)
+        db.session.add(project)
+        db.session.commit()
+
+        project_dict = {
+            'id': project.id,
+            'name': project.name,
+            'description': project.description,
+            'start_date': project.start_date,
+            'end_date': project.end_date,
+            'responsible': project.responsible,
+            'institution_id': project.institution_id
+        }
+
+        return jsonify(project_dict)
